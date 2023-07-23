66cd3cdc37a0c64de23c3c210e7ef46fcbed2a7d
0du1kenobi <am4177167@gmail.com>
2023-07-23 12:55:45 +0200

    add str numbers

--- diff_app.py
+++ diff_app.py
@@ -5,6 +5,7 @@ def main(page):
     num1 = ft.Ref[ft.TextField]()
     num2 = ft.Ref[ft.TextField]()
     results = ft.Ref[ft.Column]()
+    counter = [0]
 
     def btn_click(e):
         try:
@@ -12,15 +13,20 @@ def main(page):
             number2 = float(num2.current.value)
             diff = round(number1 - number2, 2)
             percent = round((diff / number1) * 100, 2)
+
+            counter[0] += 1  # Увеличиваем счетчик
+            # Добавляем счетчик перед текстом результата
             results.current.controls.append(
-                ft.Text(f"{number1} and {number2} - [Diff: {diff}] [{percent}%]", selectable=True)
+                ft.Text(f"{counter[0]}. {number1} and {number2} - [Diff: {diff}] [{percent}%]", selectable=True)
             )
+
             num1.current.value = ""
             num2.current.value = ""
             page.update()
             num1.current.focus()
         except ValueError:
-            results.current.controls.append(ft.Text("Invalid input. Please enter valid numbers."))
+            counter[0] += 1  # Увеличиваем счетчик
+            results.current.controls.append(ft.Text(f"{counter[0]}. Invalid input. Please enter valid numbers."))
             page.update()
 
     page.add(
